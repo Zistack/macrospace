a21850b1a3a43284aeba9a0d3d64d716679fb81c
Zistack <Zistack@null.net>
2025-08-24 15:28:11 -0400

    Added method for getting mutable path arguments even when there are none initially

--- src/path_utils.rs
+++ src/path_utils.rs
@@ -1,4 +1,10 @@
-use syn::{Path, PathArguments, GenericArgument, Token};
+use syn::{
+	Path,
+	PathArguments,
+	AngleBracketedGenericArguments,
+	GenericArgument,
+	Token
+};
 use syn::punctuated::Punctuated;
 use syn::parse::{Result, Error};
 use quote::ToTokens;
@@ -49,6 +55,53 @@ pub fn get_path_arguments_mut (path: &mut Path)
 				)
 			),
 			_ => Ok (None)
+		},
+		None => Err (Error::new_spanned (path_tokens, "Path must be nonempty"))
+	}
+}
+
+pub fn make_path_arguments (path: &mut Path)
+-> Result <&mut Punctuated <GenericArgument, Token! [,]>>
+{
+	let path_tokens = path . to_token_stream ();
+
+	match path . segments . last_mut ()
+	{
+		Some (last_segment) =>
+		{
+			let path_arguments = match &mut last_segment . arguments
+			{
+				PathArguments::AngleBracketed (arguments) => return Ok (&mut arguments . args),
+				PathArguments::Parenthesized (_) => return Err
+				(
+					Error::new_spanned
+					(
+						path_tokens,
+						"Parenthesized path arguments are not supported"
+					)
+				),
+				a @ _ => a
+			};
+
+			*path_arguments = PathArguments::AngleBracketed
+			(
+				AngleBracketedGenericArguments
+				{
+					colon2_token: None,
+					lt_token: Default::default (),
+					args: Punctuated::new (),
+					gt_token: Default::default ()
+				}
+			);
+
+			if let PathArguments::AngleBracketed (arguments) = path_arguments
+			{
+				Ok (&mut arguments . args)
+			}
+			else
+			{
+				unreachable! ();
+			}
 		},
 		None => Err (Error::new_spanned (path_tokens, "Path must be nonempty"))
 	}
