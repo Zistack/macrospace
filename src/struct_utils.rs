4d66cfaee94bb6268eb45d92cc6d612109f8ff58
Zistack <Zistack@null.net>
2025-10-05 17:24:27 -0400

    Changed constructor utility to take a path and format it correctly when it contains arguments

--- src/struct_utils.rs
+++ src/struct_utils.rs
@@ -1,20 +1,24 @@
-use syn::{Type, Fields, Member};
+use syn::{Path, Type, Fields, Member};
 use quote::{ToTokens, quote};
+
+use crate::path_utils::as_prefix;
 
 pub fn constructor <T>
 (
-	ty: &Type,
+	ty_path: &Path,
 	members: &Vec <Member>,
 	values: &Vec <T>
 )
 -> proc_macro2::TokenStream
 where T: ToTokens
 {
+	let ty_path = as_prefix (ty_path . clone ());
+
 	match members . get (0)
 	{
-		None => quote! (#ty),
-		Some (Member::Named (_)) => quote! (#ty {#(#members: #values),*}),
-		Some (Member::Unnamed (_)) => quote! (#ty (#(#values),*))
+		None => quote! (#ty_path),
+		Some (Member::Named (_)) => quote! (#ty_path {#(#members: #values),*}),
+		Some (Member::Unnamed (_)) => quote! (#ty_path (#(#values),*))
 	}
 }
 
