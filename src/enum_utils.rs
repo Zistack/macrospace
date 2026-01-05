52a5e6854c33c9d62fd8faa91f13a2f502bdeef0
Zistack <Zistack@null.net>
2026-01-05 16:55:16 -0500

    Added/renamed utilities for accessing variant data

--- src/enum_utils.rs
+++ src/enum_utils.rs
@@ -2,6 +2,12 @@ use syn::{Variant, Fields, Ident, Type, Token};
 use syn::parse::{Result, Error};
 use syn::punctuated::Punctuated;
 
+pub fn get_variant_idents (variants: &Punctuated <Variant, Token! [,]>)
+-> Vec <Ident>
+{
+	variants . iter () . map (|variant| variant . ident . clone ()) . collect ()
+}
+
 pub fn get_variant_type (variant: &Variant) -> Result <&Type>
 {
 	match &variant . fields
@@ -34,7 +40,37 @@ pub fn get_variant_types (variants: &Punctuated <Variant, Token! [,]>)
 	Ok (variant_types)
 }
 
-pub fn get_variants (variants: &Punctuated <Variant, Token! [,]>) -> Vec <Ident>
+pub fn get_variant_idents_and_types (variants: &Punctuated <Variant, Token! [,]>)
+-> Result <Vec <(Ident, Type)>>
 {
-	variants . iter () . map (|variant| variant . ident . clone ()) . collect ()
+	let mut variant_idents_and_types = Vec::new ();
+
+	for variant in variants
+	{
+		variant_idents_and_types . push
+		((
+			variant . ident . clone (),
+			get_variant_type (variant)? . clone ()
+		));
+	}
+
+	Ok (variant_idents_and_types)
+}
+
+pub fn get_variant_idents_and_types_split
+(
+	variants: &Punctuated <Variant, Token! [,]>
+)
+-> Result <(Vec <Ident>, Vec <Type>)>
+{
+	let mut variant_idents = Vec::new ();
+	let mut variant_types = Vec::new ();
+
+	for variant in variants
+	{
+		variant_idents . push (variant . ident . clone ());
+		variant_types . push (get_variant_type (variant)? . clone ());
+	}
+
+	Ok ((variant_idents, variant_types))
 }
